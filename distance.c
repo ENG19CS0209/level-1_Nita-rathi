4c56ec5f64f0d4015fd9f9f713e0259adf98f021
ENG19CS0209 <76103383+ENG19CS0209@users.noreply.github.com>
2021-02-12 13:45:26 +0530

    remember

--- distance.c
+++ distance.c
@@ -1,1 +1,56 @@
-//WAP to find the distance between two point using 4 functions.
+//WAP to find the dista#include <stdio.h>
+
+#include <math.h>
+
+
+float input()
+
+{
+
+      float n;
+
+	  printf("\n enter number:");
+
+	  scanf("%f",&n);
+
+	  return n;
+
+}
+
+float distance(float a,float b)
+
+{
+
+     float distance;
+
+	 distance =sqrt(a*a+b*b);
+
+	 return distance;
+
+}
+
+void output(float n)
+
+{
+
+     printf("\n the distance is %f:", n);
+
+}
+
+int main()
+
+{
+
+float x,y,z;
+
+x=input();
+
+y=input();
+
+z=distance(x,y)
+
+output(z);
+
+return 0;
+
+nce between two point using 4 functions.
