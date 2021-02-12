61d4166a31b926cb6355fa087390ea3e6fa538e6
ENG19CS0209 <76103383+ENG19CS0209@users.noreply.github.com>
2021-02-12 16:28:58 +0530

    final commit

--- two_fractions.c
+++ two_fractions.c
@@ -1,29 +1,69 @@
 #include<stdio.h>
-#include <math.h>
-typedef struct {
-int n;
-int d;
-}fraction;
-fraction sum (fraction f1, fraction f2)
+
+#include<math.h>
+
+typedef struct
+
 {
-fraction r={ (f1.n * f2.d) + (f2.n * f1.d), f1.d * f2.d };
-return r;
+
+  int n;
+
+  int d;
+
+
+} Fraction;
+
+
+Fraction
+
+sum (Fraction f1, Fraction f2)
+
+{
+
+  Fraction result = { (f1.n * f2.d) + (f2.n * f1.d), f1.d * f2.d };
+
+  return result;
+
 }
-fraction input()
+
+
+Fraction
+
+input (Fraction c, int a)
+
 {
-printf ("Enter fraction %d: numerator and denominator:", a);
+
+  printf ("Enter fraction %d: numerator and denominator:", a);
+
   scanf ("%d%d", &c.n, &c.d);
+
   return c;
+
 };
-void display()
+
+
+void display(Fraction r){
+
+  printf ("Result=%d/%d", r.n, r.d);
+
+}
+
+int
+
+main ()
+
 {
-printf("result =%d/%d",r.n,r.d);
+
+  Fraction f1 = input (f1, 1);
+
+  Fraction f2 = input (f2, 2);
+
+  Fraction r = sum (f1, f2);
+
+  display(r);
+
+  return 0; 
+
 }
-int main (){
-fraction f1= input (f1,1);
-fraction f2 = input (f2,2);
-fraction r = sum (f1,f2);
-display(r);
-return 0;
-}
+
 
