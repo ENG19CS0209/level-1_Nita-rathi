a5e8f8f7925d004718f48b98b2c75fa1a0668fa5
ENG19CS0209 <76103383+ENG19CS0209@users.noreply.github.com>
2021-02-12 16:27:00 +0530

    remember

--- two_fractions.c
+++ two_fractions.c
@@ -1,1 +1,29 @@
-//WAP to find the sum of two fractions.
+#include<stdio.h>
+#include <math.h>
+typedef struct {
+int n;
+int d;
+}fraction;
+fraction sum (fraction f1, fraction f2)
+{
+fraction r={ (f1.n * f2.d) + (f2.n * f1.d), f1.d * f2.d };
+return r;
+}
+fraction input()
+{
+printf ("Enter fraction %d: numerator and denominator:", a);
+  scanf ("%d%d", &c.n, &c.d);
+  return c;
+};
+void display()
+{
+printf("result =%d/%d",r.n,r.d);
+}
+int main (){
+fraction f1= input (f1,1);
+fraction f2 = input (f2,2);
+fraction r = sum (f1,f2);
+display(r);
+return 0;
+}
+
